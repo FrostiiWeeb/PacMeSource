3c45d837cd608f4558a83c752076f13cb71e650d
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2020-12-14 18:57:23 +0100

    Create join.py

---
+++ cogs/join.py
@@ -0,0 +1,55 @@
+import discord
+from discord.utils import get
+import asyncio
+from asyncio import sleep
+from asyncio import gather
+import random
+import datetime
+import platform
+from discord.ext.commands import ConversionError
+import json
+from json import dump, load
+import os
+from discord import Member
+import time
+import io
+import sys
+from io import BytesIO
+import contextlib
+import inspect
+from datetime import datetime
+import aiohttp
+from discord import Activity, ActivityType
+from discord import Member
+from discord.ext import commands
+
+class OnGuildJoin(commands.Cog):
+	
+	def __init__(self, bot):
+		self.bot = bot
+		
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+		
+	@commands.Cog.listener()
+	async def on_guild_join(self, guild):
+	    embed = discord.Embed(title='PacMe', color=discord.Colour.blurple())
+	    embed.add_field(name="What's up everyone? I am **PacMe**.", value='\nTry typing `!*help` to get started.', inline=False)
+	    embed.set_footer(text='Thanks for adding PacMe to your server!')
+	    await guild.system_channel.send(embed=embed)
+	    
+	@commands.Cog.listener()
+	async def on_guild_leave(self, ctx, guild):
+	  with open('prefixes.json', 'r') as f:
+	  	prefixes = json.load(f)
+	  	print(prefixes)
+	  
+	  del prefixes[ctx.message.guild.id]
+	  print(prefixes)
+	
+	  with open('prefixes.json', 'w') as f:
+	     	    	    json.dump(prefixes, f)	    
+	    	    	    
+def setup(bot):
+	bot.add_cog(OnGuildJoin(bot))	
