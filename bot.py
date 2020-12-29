36eafeba518be26e47742b26e7a6c4acb1452c87
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2020-12-29 10:49:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,23 +6,31 @@ from itertools import cycle
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
-from pretty_help import PrettyHelp
 
 default_prefix = "!*"
 
-def get_prefix(bot, ctx):
-    with open('./prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+f = "./prefixes.json"
+mr = 'r'
+mw = 'w'
 
-    return prefixes.get(str(ctx.guild.id), default_prefix)
+def get_prefix(bot, ctx):
+	with open(f, mr) as fl:
+		prefixes = json.load(fl)
+		
+	return prefixes.get(str(ctx.guild.id), default_prefix)
 
 color = discord.Colour.green()
-bot = commands.Bot(command_prefix = get_prefix, fetch_offline_members = True, case_insensitive=True, help_command=PrettyHelp(no_category="Default Category", color=color, hidden=['cogs.onguildjoin', 'cogs.commanderror', 'cogs.error']), intents=discord.Intents.all())
+bot = commands.Bot(command_prefix = get_prefix, fetch_offline_members = True, case_insensitive=True, intents=discord.Intents.all())
+bot.remove_command('help')
 bot.launch_time = datetime.utcnow()
 
 bot.owner_ids = {746807014658801704, 668906205799907348}
 
-status = cycle(['Commands: !*help', f'{len(bot.guilds)} servers! | !*help', 'PacMan'])
+bot.version = "1.1.0"
+
+bot.colour = discord.Colour.red()
+
+status = cycle(['Commands: !*help', f'{len(bot.guilds)} servers! | !*help', 'PacMan'])		
 @bot.event
 async def on_ready():
     await bot.wait_until_ready()
@@ -44,9 +52,15 @@ async def on_ready():
     print('')
     print('                                            Version:')
     print(f"                                             {discord.__version__}")
-    print ('                              ================================================')
+    print ('                              ================================================')  
+    log_channel = bot.get_channel(789892435190349859)
+    embed = discord.Embed(
+    colour = discord.Colour.blurple(),
+    title = f"{bot.user.name} Is online!")
+    embed.set_footer(text="Howdy' how ya'll doin'?", icon_url=bot.user.avatar_url)
+    await log_channel.send(embed=embed)
     class colors:
-        default = 0
+    	default = 0
     teal = 0x1abc9c
     dark_teal = 0x11806a
     green = 0x2ecc71
@@ -72,27 +86,50 @@ async def on_ready():
 
 @tasks.loop(seconds=10)
 async def change_status():
-      await bot.change_presence(activity=discord.Game(next(status)))
-
+	  await bot.change_presence(activity=discord.Game(next(status)))
+	        	
 @bot.command(aliases=["src"])
 async def source(ctx):
-    embed = discord.Embed(colour = discord.Colour.blurple(), title = "Heres the source!", description="[Here!](https://github.com/FrostiiWeeb/PacMeSource/)")
-    embed.add_field(name="Leave a star, if u use my code!", value="Enjoy!")
-    await ctx.send(embed=embed)
+	embed = discord.Embed(colour = discord.Colour.blurple(), title = "Heres the source!", description="[Here!](https://github.com/FrostiiWeeb/PacMeSource/)")
+	embed.add_field(name="Leave a star, if u use my code!", value="Enjoy!")
+	await ctx.send(embed=embed)
+
+@bot.command(aliases=['r'])
+@commands.is_owner()
+async def restart(ctx):
+	log_channel = bot.get_channel(789892435190349859)        	   
+	embed = discord.Embed(
+	colour = discord.Colour.blurple(),
+	title = f"{bot.user.name} Restarting!"
+	)    
+	embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+	embed.set_footer(text="Howdy! Ill be restarting in a few seconds.", icon_url=bot.user.avatar_url)	  
+	await log_channel.send(embed=embed)
+	await ctx.send(embed=embed)
+	await ctx.bot.logout()
+
+@bot.command(aliases=['cl'])
+async def changelog(ctx):
+	embed = discord.Embed(
+	colour = bot.colour,
+	title = "**Changelog:**",
+	description=f"```Added type racer command.```\n```Added Fun Cog.```"
+	)
+	await ctx.send(embed=embed)
 
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 
-os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
 os.environ["JISHAKU_HIDE"] = "True"
-
-
-bot.load_extension('jishaku')
-
+        	    						    						
+        	    						     						
+bot.load_extension('jishaku')   	    	    
+	   	                                 
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py') and not filename.startswith('_'):
     bot.load_extension(f'cogs.{filename[:-3]}')
 
-
+    
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv('DISCORD_TOKEN')      
 bot.run(token)
